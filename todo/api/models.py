c1a4aa26a7fd3bd8887efa759683465fc05ac578
filipelbc <filipelbc@gmail.com>
2019-03-21 22:45:53 -0300

    Add django admin

--- todo/api/models.py
+++ todo/api/models.py
@@ -5,6 +5,9 @@ class Label(models.Model):
 
     name = models.CharField(max_length=255, unique=True)
 
+    def __str__(self):
+        return 'Label(id=%s, name=%s)' % (self.id, repr(self.name))
+
 
 class Item(models.Model):
 
@@ -13,3 +16,6 @@ class Item(models.Model):
     description = models.TextField(default='')
 
     labels = models.ManyToManyField(Label)
+
+    def __str__(self):
+        return 'Item(id=%d, title=%s)' % (self.id, repr(self.title))
