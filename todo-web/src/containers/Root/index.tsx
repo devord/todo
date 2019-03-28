c1b765f940fe7f387a79f62b09c8ff086c3b790e
Daniel Moraes <daniel.b.moraes@gmail.com>
2019-03-28 12:16:10 -0300

    Project structure and initial content.

---
+++ todo-web/src/containers/Root/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Root = () => (
+  <React.Fragment>
+    TODO
+  </React.Fragment>
+)
+
+export default Root
--- todo-web/src/index.tsx
+++ todo-web/src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import { Root } from 'src/containers'
+
+import * as serviceWorker from './serviceWorker'
+import './index.css'
+
+ReactDOM.render(<Root />, document.getElementById('root'))
 
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister()
